c3b56e925cb574837d65fd8a5fcc6c7296a0ace2
Priyansh Gupta <priyansh7533053411@gmail.com>
2019-09-17 16:42:21 +0530

    Update

--- Ellipse Algortihms/Polar_Ellipse.cpp
+++ Ellipse Algortihms/Polar_Ellipse.cpp
@@ -1,52 +1,50 @@
-	#include<stdio.h>
-	#include<GL/glut.h>
-	#include<math.h>
-	#include<stdlib.h>
-
-	int x1,yr,r;
-
-	void Polar_Ellipse(void){
-	
-	int i,x,y;
-	float theta;
-	
-	for(i=0;i<=360;i++){
-		theta=(i*3.14)/180;
-		x=(int)(x1+(r*cos(theta)));
-		y=(int)(yr+(r*sin(theta)));
-		glBegin(GL_POINTS);
-		glVertex2d(x,y);
-		glEnd();
-	}
-	
-	glFlush();
-	}
-
-	void init(void)
-	{
-	glClearColor(1.0, 14.0, 51.0, 0.0);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluOrtho2D(-100,100,-100,100);
-	}
-
-	int main(int argc, char** argv) {
-	printf("Enter the value of x1: ");
-	scanf("%d",&x1);
-	printf("Enter the value of y1: ");
-	scanf("%d",&yr);
-	printf("Enter the value of radius of circle: ");
-	scanf("%d",&r);
-
-	glutInit(&argc, argv);
-	glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowSize (500, 500);
-	glutInitWindowPosition (100,100);
-	glutCreateWindow ("Circle Using Polar Co-ordinates");
-	init();
-	glutDisplayFunc(Polar_Ellipse);
-	glutMainLoop();
-
-	return 0;
-	}
-
+#include <GL/glut.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+int h,k;
+float a,b;
+void display(void)   {
+   double I,J;
+    int i,j;
+    glClear (GL_COLOR_BUFFER_BIT);
+    glColor3f (1.0, 0.0, 0.0);
+    glBegin(GL_POINTS);
+    glVertex2s(h,k);
+    for( i=0 ; i<=a ; i+=1)     {
+        J=sqrt(1 - (i*i)/(a*a))*b;
+        j=(int)(J);
+glVertex2s(h+i,k+j);
+        glVertex2s(h-i,k+j);
+        glVertex2s(h-i,k-j);
+        glVertex2s(h+i,k-j);       }
+    glColor3f (1.0, 1.0, 1.0);
+    for(int i=-100 ; i<=100 ; i++)
+    {     glVertex2s(i,0);
+        glVertex2s(0,i);        }
+    for(int i=-2; i<=2 ; i++)
+    {      glVertex2s(95+i,4+i);
+        glVertex2s(95-i,4+i);     }
+    for(int i=0; i<=2 ; i++)
+    {      glVertex2s(4+i,95+i);
+        glVertex2s(4-i,95+i);
+        glVertex2s(4,95-i);	  }
+    glEnd();
+    glFlush();	}
+void init(void)	{
+    glClearColor (1.0, 1.0, 1.5, 0.0);
+    glOrtho(-100.0, 100.0, -100.0, 100.0, -1.0, 1.0);		}
+int main(int argc, char** argv)	{
+    printf("Enter the center of ellipse:\n");
+    scanf("%d %d",&h,&k);
+    printf("Enter the parameters a & b:\n");
+    scanf("%f %f",&a,&b);
+    glutInit(&argc, argv);
+    glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize (500, 500);
+    glutInitWindowPosition (100, 100);
+    glutCreateWindow ("Ellipse : mid-point ");
+    init ();
+    glutDisplayFunc(display);
+    glutMainLoop();
+    return 0;			}
